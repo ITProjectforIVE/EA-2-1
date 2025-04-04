47ad42542e1657aad44b83f6273d6f12b394a643
CMH10102001 <140772597+CMH10102001@users.noreply.github.com>
2025-04-05 00:51:07 +0800

    Add files via upload

--- Quest.js
+++ Quest.js
@@ -1,8 +1,62 @@
-function ChooseSide(){
-let west = document.getElementById("West");
-let east = document.getElementById("East");
-let asia = document.getElementById("Asia");
-west.innerHTML('west');
-east.innerHTML('');
-asia.innerHTML('');
+function Westcountry(){
+let c = prompt("Please input country name",""); //show prompt box
+let line = "You have choose " + c +"!\n"; //show country name
+	if (c == "British" || c == "France"){
+	    alert(line+" Defend your homeland and counter attack the Axis!");
+	}
+    else if(c =="Germany"||c == "Italy"){
+        alert(line+" Invade other country as soon as possible!");
+    }
+    else if(c== "USA"){
+        alert("Help British and France land on the Europe!");
+    }
+    else if(c == null){
+        return false;
+    }
+    else{
+        alert("Invalid country name!");
+        return Westcountry();
+    }
+}
+
+function Eastcountry(){
+let c = prompt("Please input country name","");
+let line = "You have choose " + c +"!\n";
+    if (c == "Soviet Union"){
+	    alert(line+"Defend your homeland and counter attack the Berlin!");
+	}
+    else if(c=="Germany"){
+        alert(line+"Invade Moscow!");
+    }
+    else if(c=="Finland"){
+        alert(line+"Protect your country between Soviet Union and Germany!");
+    }
+    else if(c == null){
+        return false;
+    }
+    else{
+        alert("Invalid country name!");
+        return Eastcountry();
+    }
+}
+
+function Asiacountry(){
+    let c = prompt("Please input country name","");
+    let line = "You have choose " + c +"!\n";
+        if (c == "China"||c =="Soviet Union"){
+            alert(line+"Defend your homeland!");
+        }
+        else if(c=="USA"){
+            alert(line+"Try to land on Japan and  nuke!");
+        }
+        else if(c=="Japan"){
+            alert(line+"Invade the China and Pacific Islands!");
+        }
+        else if(c == null){
+            return false;
+        }
+        else{
+            alert("Invalid country name!");
+            return Asiacountry();
+        }
 }
